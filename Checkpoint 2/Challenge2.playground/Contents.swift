22c57a54cbc17c67f05124c503e1713c12b65acb
Servontius Turner <38194637+ServontiusT@users.noreply.github.com>
2022-05-13 18:37:44 -0500

    Challenge 2 Initial Upload

---
+++ Checkpoint 2/Challenge2.playground/Contents.swift
@@ -0,0 +1,6 @@
+let languages = ["Python", "Swift", "JavaScript", "Swift", "C++", "Python"]
+
+let languageCount = languages.count
+let uniqueLanguageCount = Set(languages).count
+
+print("The number of values in the Array are: \(languageCount), and the number of unique values are: \(uniqueLanguageCount)")
---
+++ Checkpoint 2/about.md
@@ -0,0 +1,3 @@
+# Challenge 2
+
+This challenge was presented after learning about arrays, dictionaries and sets. This challenge involves creating an array of strings then writing code that prints the unique items in the array.
